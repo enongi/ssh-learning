ad3b3b80b4e7739b44bcc15356796ab8e989a0ca
lizhenzhong <lzzqqtxfh@163.com>
2018-08-24 10:27:53 +0800

    First Commit!

---
+++ chapter1-1-2/src/main/java/com/chapter112/entity/TbUser.java
@@ -0,0 +1,115 @@
+package com.chapter112.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user", schema = "framework-learning", catalog = "")
+public class TbUser {
+    private int id;
+    private String username;
+    private String nickname;
+    private String password;
+    private Boolean sex;
+    private Integer age;
+    private String email;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 32)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "nickname", nullable = true, length = 32)
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 32)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "sex", nullable = true)
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    @Basic
+    @Column(name = "age", nullable = true)
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 64)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TbUser tbUser = (TbUser) o;
+
+        if (id != tbUser.id) return false;
+        if (username != null ? !username.equals(tbUser.username) : tbUser.username != null) return false;
+        if (nickname != null ? !nickname.equals(tbUser.nickname) : tbUser.nickname != null) return false;
+        if (password != null ? !password.equals(tbUser.password) : tbUser.password != null) return false;
+        if (sex != null ? !sex.equals(tbUser.sex) : tbUser.sex != null) return false;
+        if (age != null ? !age.equals(tbUser.age) : tbUser.age != null) return false;
+        if (email != null ? !email.equals(tbUser.email) : tbUser.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (sex != null ? sex.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
